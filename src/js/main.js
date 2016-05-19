7e89302cc76587fdbc1165c6d0a1489adecf2ddb
Nisa Sanyang <misramz@yahoo.com>
2016-05-19 14:38:49 -0400

    api appended just need to import pic and style

--- src/js/main.js
+++ src/js/main.js
@@ -12,11 +12,27 @@ let basicInfo = $('.info');
 //data from URL link
 var html;
 $.getJSON(url).then(function(res){
-  //console.log(res.name);
+  console.log(res);
   var html = title(res.name);
   name.append(html);
+
+  var htmlBasic = basics(res)
+  basicInfo.append(htmlBasic);
 });
 
 function title(name){
   return `<h1>${name}</h1>`;
+};
+
+function basics(namesmall){
+  console.log('namesmall', namesmall)
+  return `
+    <div class="info">
+      <h4> Name : ${namesmall.name}</h4>
+      <h4> GitHub :<a href="${namesmall.html_url}">${namesmall.login}</a> </h4>
+      <h4> Email: ${namesmall.email}</h4>
+      <h4> Company : ${namesmall.company}</h4>
+      <h4> Website : ${namesmall.url}</h4>
+    </div>
+  `;
 }
